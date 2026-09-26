4c3da7e0e61cb4f1e24951329e528fbdb6d6a691
y9203952-droid <y9203952@gmail.com>
2026-09-26 11:16:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // shared memories, photo uploads, invites, and cloud syncing.
 
 export const SUPABASE_URL = "https://mystxodvzisfwvtofllf.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15c3R4b2R2emlzZnd2dG9mbGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNDQ4ODcsImV4cCI6MjEwNTkyMDg4N30.RMN-JBscqFwsik02YPnUAvGcNjH7mPJu8wls1OEdhUM";
